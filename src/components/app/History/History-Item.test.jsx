8a43f4b91aea88fb680605288182811ea1c1f39d
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-10-23 11:44:46 -0700

    History-Item w/ test

---
+++ src/components/app/History/History-Item.test.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import HistoryItem from './History-Item';
+
+describe('HistoryItem component', () => {
+  afterEach(() => cleanup());
+  it('render HistoryItem component', () => {
+    const { asFragment } = render(<HistoryItem
+      url="https://placekitten.com/"
+      method="GET"
+    />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
